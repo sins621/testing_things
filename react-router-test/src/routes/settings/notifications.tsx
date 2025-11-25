8ff836bfa5031f3b4d3c5ecbe11b8565d95cacf9
sins <sinsmailza@gmail.com>
2025-11-25 14:03:16 +0200

    rename

--- react-router-test/src/routes/settings/notifications.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/settings/notifications')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/settings/notifications"!</div>
-}
--- react-router-test/src/routes/test.nested.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/test/nested')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/test/nested"!</div>
-}
