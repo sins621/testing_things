f7523e9b697de75154b936b6b11bcca03a53eba0
sins <sinsmailza@gmail.com>
2025-11-26 19:41:30 +0200

    more params

--- tanstack-router-test/src/routes/params/index.tsx
+++ tanstack-router-test/src/routes/params/index.tsx
@@ -1,31 +1,57 @@
-import { createFileRoute, Link, MatchRoute } from '@tanstack/react-router'
+import {
+  createFileRoute,
+  Link,
+  MatchRoute
+} from '@tanstack/react-router'
+import { Suspense, use } from 'react'
 import { z } from 'zod'
 
 const postsSchema = z.object({
   userId: z.number().default(1).catch(1),
 })
 
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 export const Route = createFileRoute('/params/')({
   validateSearch: postsSchema,
+  beforeLoad: () => ({
+    fetchPosts: async (userId: number) => {
+      await sleep(3_600)
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+      )
+      if (!res.ok) throw new Error('Failed to fetch data')
+      return res.json()
+    },
+  }),
   loaderDeps: ({ search: { userId } }) => ({ userId }),
-  loader: async ({ deps: { userId } }) => {
-    const res = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
-    )
-    if (!res.ok) throw new Error('Failed to fetch data')
-    return res.json()
+  loader: async ({ context: { fetchPosts }, deps: { userId } }) => {
+    return { slowData: fetchPosts(userId) }
   },
   component: RouteComponent,
+  errorComponent: () => <div>Error</div>,
 })
 
 function RouteComponent() {
-  const data = Route.useLoaderData()
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SuspensedComponent />
+    </Suspense>
+  )
+}
+
+function SuspensedComponent() {
+  const { slowData } = Route.useLoaderData()
+  const data = use(slowData)
+
   return (
     <>
       <div>{JSON.stringify(data)}</div>
       <Link
         from={Route.fullPath}
-        search={(prev) => ({ userId: prev.userId + 1 })}
+        search={(prev) => ({ userId: (prev.userId ?? 0) + 1 })}
       >
         Next User
         <MatchRoute pending to={Route.fullPath}>
